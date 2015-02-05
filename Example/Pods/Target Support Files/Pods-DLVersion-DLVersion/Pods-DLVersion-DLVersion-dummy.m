ed5d854e24f1833a186608f193fb33a6d3442998
Nate West <nwest@detroitlabs.com>
2015-02-05 15:56:22 -0500

    Remove checked in dependencies for Example app

--- Example/Pods/Target Support Files/Pods-DLVersion-DLVersion/Pods-DLVersion-DLVersion-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DLVersion_DLVersion : NSObject
-@end
-@implementation PodsDummy_Pods_DLVersion_DLVersion
-@end
--- Example/Pods/Target Support Files/Pods-Tests-DLVersion/Pods-Tests-DLVersion-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_DLVersion : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_DLVersion
-@end
